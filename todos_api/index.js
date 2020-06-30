8833c216dbba8ab0eb5e0616cbaabc1342f611dd
Daria Korbysheva <dariakorbysheva@MacBook-Air.local>
2020-06-30 00:16:27 -0700

    added index file, css and app js for front-end

--- todos_api/index.js
+++ todos_api/index.js
@@ -6,10 +6,13 @@ bodyParser = require("body-parser");
 var todoRoutes = require('./routes/todos')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(express.static(__dirname +"/views"));
+
 
 app.get('/', function(req, res){
-    res.send("HI, THERE FROM ROOT ROUTE!")
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
---
+++ todos_api/public/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+ $.getJSON("/api/todos")
+ .then(addTodos)
+ .catch(function(err){
+     console.log(err)
+ })
+});
+
+function addTodos(todos){
+    todos.forEach(function(todo){
+       var newTodo = $('<li class="task">' + todo.name + '</li>');
+       if(todo.completed){
+           newTodo.addClass("done");
+       }
+       $(".list").append(newTodo);
+    })
+
+}
